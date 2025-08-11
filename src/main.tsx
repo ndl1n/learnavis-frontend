83036f30eedd8dddfdd1c48d1f7e7d5b35ac5ded
ndl1n <a0938391667@gmail.com>
2025-08-12 01:57:15 +0800

    add main

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { RouterProvider } from 'react-router';
+import router from './router.tsx';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
